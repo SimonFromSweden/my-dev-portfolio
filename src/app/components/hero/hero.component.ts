40cf941ff518f9c1c872bc603b7eaaf0c41c7183
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-08-26 11:15:07 +0200

    feat: fixing heroslider buttons and functionality + 3 test images

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface HeroImage {
+  src: string;
+  alt: string;
+}
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  images: HeroImage[] = [
+    {
+      src: 'images/hero-test.jpg',
+      alt: 'ocean',
+    },
+    {
+      src: 'images/hero-test2.jpg',
+      alt: 'splash of color',
+    },
+    {
+      src: 'images/hero-test3.jpg',
+      alt: 'splash of color 2',
+    },
+  ];
+
+  currentIndex: number = 0;
+
+  fadingOut: boolean = false;
+
+  nextImage(): void {
+    this.fadingOut = true;
+    setTimeout(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      this.fadingOut = false;
+    }, 300);
+  }
+
+  prevImage(): void {
+    this.fadingOut = true;
+    setTimeout(() => {
+      this.currentIndex =
+        this.currentIndex === 0
+          ? this.images.length - 1
+          : this.currentIndex - 1;
+      this.fadingOut = false;
+    }, 300);
+  }
+}
